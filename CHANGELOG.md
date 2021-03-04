37a4870b954e35f775d7ad8339e76b798eac5a53
Andrey Petrov <andrey@petrov.works>
2021-03-04 08:34:32 +0300

    Keep moving torwards normal scanner -> filter -> parser chain.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 # Changelog for bottomless
 
 ## Unreleased changes
+### Added
+- API to parse data from TOML v1.0.0 documents
+- API to query data from parsed structures
--- src/TOML/Scanner.hs
+++ src/TOML/Scanner.hs
@@ -2,33 +2,64 @@ module TOML.Scanner (Constant(..), Token(..), scan) where
 
 data Constant = AnyInt Int | AnyString String deriving Show
 
-data Token = Comment String | Key String | Syntax Char | Payload Constant deriving Show
+data Token = Key String
+           | Syntax Char
+           | Payload Constant
+           deriving Show
 
 data State = Idle 
+           | Comment
+           | CommentInput
+           deriving (Show, Eq)
 
-data Mask = Mask State String [Token]
+type Scope = String
+
+data Mask = Mask State Scope [Token]
 
 scan :: String -> [Token] 
-scan source = walk source $ Mask state accumulator result 
+scan source = walk source $ Mask state scope result 
   where
-    state       = Idle 
-    accumulator = ""
-    result      = []
+    state  = Idle
+    scope  = ""
+    result = []
 
 walk :: String -> Mask -> [Token]
 walk [] mask             = terminate mask
 walk [input] mask        = terminate $ process input mask
 walk (input : rest) mask = walk rest $ process input mask
 
-process :: Char -> Mask -> Mask
-process input mask = mask
-
-move :: State -> Mask -> Mask
-move state (Mask _ _ result) = Mask state accumulator result
-  where
-    accumulator = ""
-
 terminate :: Mask -> [Token]
-terminate mask = result 
-  where
-    (Mask _ _ result) = mask
+terminate (Mask state scope result) =
+  case state of
+    Idle         -> result 
+    Comment      -> result
+    CommentInput -> result 
+
+process :: Char -> Mask -> Mask
+process input mask =
+  case input of
+    '#'  -> processComment mask 
+    '='  -> processAny mask
+    ' '  -> processSpace mask
+    '"'  -> processAny mask
+    '\'' -> processAny mask
+    '\\' -> processAny mask
+    '\n' -> processLineEnd mask
+    _    -> processAny mask 
+
+processComment :: Mask -> Mask
+processComment (Mask state scope result) 
+  | state == Idle = Mask Comment scope result
+  | otherwise = Mask state scope result
+
+processCommentInput :: Mask -> Mask
+processCommentInput mask = mask
+
+processSpace :: Mask -> Mask
+processSpace mask = mask
+
+processLineEnd :: Mask -> Mask
+processLineEnd mask = mask
+
+processAny :: Mask -> Mask
+processAny mask = mask
